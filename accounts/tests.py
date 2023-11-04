269d1ca3ec1bdd1527be51663d6090025428988f
Alinda Fortunate <alindafortunate5@gmail.com>
2023-11-04 19:36:26 +0300

    Added Signup Page Test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+
+from .forms import CustomUserCreationForm
+from .views import SignupPageView
 
 
 # Create your tests here.
@@ -32,3 +36,29 @@ class SignupPageTest(TestCase):
     def setUp(self):
         url = reverse("signup")
         self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "registration/signup.html")
+        self.assertContains(self.response, "Sign Up")
+        self.assertNotContains(self.response, "Hi there! I should not be on this page.")
+
+
+class SignupPageTests(TestCase):
+    def setUp(self):
+        url = reverse("signup")
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "registration/signup.html")
+        self.assertContains(self.response, "Sign Up")
+        self.assertNotContains(self.response, "Hi there! I should not be on this page.")
+
+    def test_signup_form(self):
+        form = self.response.context.get("form")
+        self.assertIsInstance(form, CustomUserCreationForm)
+
+    def test_signup_view(self):
+        view = resolve("/accounts/signup/")
+        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
