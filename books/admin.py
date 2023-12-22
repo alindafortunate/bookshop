3a3b6ad508b8e1792d93d1f61cbe26dad5fe301b
Alinda Fortunate <alindafortunate5@gmail.com>
2023-12-22 11:00:20 +0300

    Added media(User uploaded files)

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
-from .models import Books
+from .models import Books, Review
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
 
 
 class BookAdmin(admin.ModelAdmin):
+    inlines = [
+        ReviewInline,
+    ]
     list_display = (
         "title",
         "author",
--- books/tests.py
+++ books/tests.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Books
+from .models import Books, Review
 
 
 # Create your tests here.
